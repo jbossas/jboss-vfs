14e1262315d683234fc4b3420d9733f068cae21e
Ales Justin <ales.justin@gmail.com>
2008-06-06 09:37:40 +0000

    javadocs

--- src/main/java/org/jboss/virtual/VFSUtils.java
+++ src/main/java/org/jboss/virtual/VFSUtils.java
@@ -52,6 +52,7 @@ import org.jboss.virtual.spi.VirtualFileHandler;
  * VFS Utilities
  * 
  * @author <a href="adrian@jboss.com">Adrian Brock</a>
+ * @author <a href="ales.justin@jboss.com">Ales Justin</a>
  * @version $Revision: 1.1 $
  */
 public class VFSUtils
